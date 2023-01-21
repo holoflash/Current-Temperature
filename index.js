1e76a3e8570479c508d50448e274f873ab139469
holoflash <mikepalacemusic@gmail.com>
2023-01-21 12:24:27 +0100

    Create UI, add country flags and local time

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const container = document.querySelector('.container')
-
-const domDisplay = (aSearchResult) => {
+const domDisplay = (aSearchResult, temperature, flag, localTime) => {
     const searchResult = document.createElement('div');
     searchResult.classList.add('searchResult')
 
@@ -12,73 +10,145 @@ const domDisplay = (aSearchResult) => {
         searchResult.append(state)
     }
 
-    const country = document.createElement('span').textContent = aSearchResult.country + ", ";
+    const country = document.createElement('span').textContent = aSearchResult.country + " ";
     searchResult.append(country)
 
-    container.append(searchResult)
+    const flagIcon = document.createElement('img');
+    flagIcon.crossOrigin = 'anonymous'
+    flagIcon.style.height = '15px'
+    flagIcon.src = flag;
+    searchResult.append(flagIcon)
 
-    searchResult.addEventListener('click', () => {
-        let latitude = aSearchResult.latitude.toFixed(2);
-        let longitude = aSearchResult.longitude.toFixed(2);
-        getWeather(latitude, longitude)
-    })
+    input.classList.add('hidden')
+    searchBtn.classList.add('hidden')
+    inputAndSearch.append(searchResult)
+
+
+    searchResult.addEventListener('click', (event) => {
+        const myCity = { aSearchResult };
+        localStorage.setItem("myCity", JSON.stringify(myCity));
+        let red = (temperature + 40) / 80 * 255;
+        let blue = (80 - temperature) / 80 * 255;
+        const root = document.documentElement.style;
+        root.setProperty("--red", red);
+        root.setProperty("--blue", blue);
+        root.setProperty("--color", 'orange');
+        const gradient = document.querySelector('.gradient');
+        gradient.classList.toggle('active');
+
+        inputAndSearch.remove();
+
+        const temperatureDisplay = document.createElement('div');
+        const tempLabel = document.createElement('div');
+        const timeDisplay = document.createElement('div');
+
+        timeDisplay.classList.add('timeDisplay')
+        tempLabel.classList.add('tempLabel')
+        temperatureDisplay.classList.add('temperatureDisplay');
+
+        timeDisplay.textContent = localTime
+        tempLabel.textContent = event.target.textContent;
+        temperatureDisplay.textContent = temperature + "°C";
+
+        document.body.append(timeDisplay, tempLabel, temperatureDisplay)
+
+    });
 }
 
-const tempDisplay = (temperature) => {
-    const temperatureDisplay = document.createElement('div').textContent = "c" + temperature;
-    document.querySelector('.container').append(temperatureDisplay);
-}
+document.body.classList.add('gradient')
+const inputAndSearch = document.createElement('div');
+const input = document.createElement('input');
+const searchBtn = document.createElement('button');
+
+inputAndSearch.append(input, searchBtn);
+document.body.append(inputAndSearch);
+
+inputAndSearch.classList.add('inputAndSearch')
+
+input.placeholder = 'Search for city'
+
+searchBtn.textContent = 'search'
+searchBtn.addEventListener('click', () => {
+    let cleanSearch = input.value.replace(/[^a-zåäöA-Z\s]/g, "");
+    search(cleanSearch)
+})
+
 
 const search = async (searchTerm) => {
-    console.log(searchTerm)
     try {
         const res = await fetch(
             `https://geocoding-api.open-meteo.com/v1/search?name=${searchTerm}`
         );
         const data = await res.json();
-
-        for (let key = 0; key < Object.keys(data).length; key++) {
+        for (let key = 0; key < Object.keys(data.results).length; key++) {
             const searchResult = {
                 city: data.results[key].name,
                 country: data.results[key].country,
                 state: data.results[key].admin1,
                 latitude: data.results[key].latitude,
                 longitude: data.results[key].longitude,
-                message: data.results,
+                country_code: data.results[key].country_code,
+                timezone: data.results[key].timezone
             }
-            domDisplay(searchResult)
+            const temperature = await getTemperature(searchResult.latitude, searchResult.longitude);
+            const flag = await getFlag(searchResult.country_code);
+            const localTime = getLocalTime(searchResult.timezone);
+            domDisplay(searchResult, temperature, flag, localTime)
         }
     }
     catch (error) {
+        input.value = ''
+        input.placeholder = 'city not found'
         console.log(error)
     }
 };
 
-
-const getWeather = async (lat, long) => {
+const getTemperature = async (lat, long) => {
     try {
         const res = await fetch(
             `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&current_weather=true&`
         );
         const data = await res.json();
-        tempDisplay(data.current_weather.temperature)
+        return data.current_weather.temperature
     }
     catch (error) {
         console.log(error)
     }
 }
 
+const getFlag = async (country_code) => {
+    try {
+        const res = await fetch(
+            `https://countryflagsapi.com/svg/${country_code}`
+        );
+        return res.url
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
 
-const input = document.createElement('input')
-document.body.append(input);
+const getLocalTime = (timeZone) => {
+    let date = new Date();
+    let options = { timeZone: timeZone, hour12: false, hour: "numeric", minute: "numeric", second: "numeric" };
+    return date.toLocaleString("en-US", options);
+}
 
-const searchBtn = document.createElement('button')
-document.body.append(searchBtn)
 
-searchBtn.textContent = 'search'
+const getLocalStorage = async () => {
+    const temperature = await getTemperature(myCity.aSearchResult.latitude, myCity.aSearchResult.longitude);
+    const flag = await getFlag(myCity.aSearchResult.country_code);
+    const localTime = getLocalTime(myCity.aSearchResult.timezone);
+    domDisplay(myCity.aSearchResult, temperature, flag, localTime)
+}
 
-searchBtn.addEventListener('click', () => {
-    let cleanSearch = input.value.replace(/[^a-zA-Z\s]/g, "");
-    search(cleanSearch)
+let myCity = JSON.parse(localStorage.getItem("myCity")) || {};
 
+if (Object.keys(myCity).length > 0) {
+    getLocalStorage()
+}
+
+document.body.addEventListener('dblclick', () => {
+    localStorage.clear();
+    location.reload();
 })
